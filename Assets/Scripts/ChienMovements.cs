cbecd64060de1b50880c34c760a23cfc886b92e8
Fred Bordel <frederique.bordeleau@gmail.com>
2023-02-17 18:45:19 -0500

    trying stuff for chien following

--- Assets/Scripts/ChienMovements.cs
+++ Assets/Scripts/ChienMovements.cs
@@ -9,16 +9,27 @@ public class ChienMovements : MonoBehaviour
     public Collider2D brumeCollider;
     public Collider2D chienCollider;
     private Transform brume;
+    //public Rigidbody2D rb;
 
     private void Start()
     {
+        //rb = GetComponent<Rigidbody2D>();
         brume = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-     private void FixedUpdate()
+
+     private void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, brume.position, speed * Time.deltaTime);
+        //triggerChienFollow();
         Physics2D.IgnoreCollision(brumeCollider, chienCollider, true);
+    }
+
+    private void triggerChienFollow()
+    {
+        //var directionToPlayer = (brume.transform.position - transform.position).normalized;
+        //rb.MovePosition(brume.transform.position);
+        //rb.velocity = new Vector2(directionToPlayer.x, directionToPlayer.y) * speed;
     }
     //private Rigidbody2D rb;
     //private Player brume;
