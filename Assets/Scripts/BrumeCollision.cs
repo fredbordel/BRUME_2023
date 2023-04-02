6c443ec76cf6838cd1e96722b21f5b5309fd42be
Fred Bordel <frederique.bordeleau@gmail.com>
2023-04-02 12:31:45 -0400

    Creation nouvelle scene personnes agees

---
+++ Assets/Scripts/BrumeCollision.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrumeCollision : MonoBehaviour
+{
+
+ void OnTriggerEnter2D(Collider2D other) 
+ {
+ 
+ }
+
+}
