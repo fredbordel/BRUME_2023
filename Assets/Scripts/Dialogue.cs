ef936b3dca5efc6e98b7a63b0d1b41fe86472b43
Fred Bordel <frederique.bordeleau@gmail.com>
2023-08-05 12:45:35 -0400

    New working logic for dialogue + dialogue UI tests

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization;
+
+[System.Serializable]
+public class Dialogue
+{
+    public string name;
+    public LocalizedString[] localizedSentences; 
+}
+
