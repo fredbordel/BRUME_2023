5b494302219141237e667261238fd7d33207fd15
Fred Bordel <frederique.bordeleau@gmail.com>
2023-08-05 18:41:16 -0400

    Working joystick and keyboard movements

--- Assets/Scripts/BrumeMovements.cs
+++ Assets/Scripts/BrumeMovements.cs
@@ -1,60 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BrumeMovements : MonoBehaviour
 {
-    public float charMovingSpeed = 5.0f;
-    public Rigidbody2D rb;
-    public Animator animator;
-    public SpriteRenderer spriteRenderer;
-    Vector2 charDirection;
-    int directionValue = 0; // Idle = 0, Side = 1, Bas = 2, Haut = 3
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Rigidbody2D rbody;
+    private PlayerInputActions playerActions;
+    private Vector2 moveInput;
+
+    void Awake()
     {
-        HandleKeys();
-        GenerateMovement();
+        playerActions = new PlayerInputActions();
     }
 
-    public void HandleKeys()
+    private void OnEnable()
     {
-         if (Input.GetKey(KeyCode.UpArrow)) 
-        {
-            charDirection = Vector2.up;
-            directionValue = 3;
-        }
-
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            charDirection = Vector2.right;
-            directionValue = 1;
-            spriteRenderer.flipX = true;
-        }
-
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            charDirection = Vector2.down;
-            directionValue = 2;
-        }
-
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            charDirection = Vector2.left;
-            directionValue = 1;
-            spriteRenderer.flipX = false;
-        }
-
-        else
-        {
-            charDirection = Vector2.zero;
-        }
+        playerActions.Brume.Enable();
     }
 
-    public void GenerateMovement()
+    private void OnDisable()
     {
-        rb.MovePosition(rb.position + charDirection * charMovingSpeed * Time.fixedDeltaTime);
-        animator.SetInteger("direction", directionValue);
+        playerActions.Brume.Disable();
     }
+
+    void FixedUpdate()
+    {
+        moveInput = playerActions.Brume.Movement.ReadValue<Vector2>();
+        // CHANGE DIRECTION VALUE WITH MOVEINPUT VALUE
+        Debug.Log("HELLO " + moveInput);
+        rbody.velocity = moveInput * speed;
+    }
+
+
+    // public float charMovingSpeed = 5.0f;
+    // public Rigidbody2D rb;
+    // public Animator animator;
+    // public SpriteRenderer spriteRenderer;
+    // Vector2 charDirection;
+    // int directionValue = 0; // Idle = 0, Side = 1, Bas = 2, Haut = 3
+
+
+    // [SerializeField]
+    // private InputActionReference movement;
+    // private Vector2 movementInput;
+
+    // void Update()
+    // {
+    //     movementInput = movement.action.ReadValue<Vector2>();
+    //     HandleKeys();
+    //     GenerateMovement();
+    // }
+
+    // private void OnEnable()
+    // {
+    //     movement.action.performed += LogIt;
+    // }
+
+    // private void LogIt(InputAction.CallbackContext obj)
+    // {
+    //     Debug.Log("ACTION PERFORMED" + obj);
+    // }
+
+    // public void HandleKeys()
+    // {
+    //      if (Input.GetKey(KeyCode.UpArrow)) 
+    //     {
+    //         charDirection = Vector2.up;
+    //         directionValue = 3;
+    //     }
+
+    //     else if (Input.GetKey(KeyCode.RightArrow) )
+    //     {
+    //         charDirection = Vector2.right;
+    //         directionValue = 1;
+    //         spriteRenderer.flipX = true;
+    //     }
+
+    //     else if (Input.GetKey(KeyCode.DownArrow))
+    //     {
+    //         charDirection = Vector2.down;
+    //         directionValue = 2;
+    //     }
+
+    //     else if (Input.GetKey(KeyCode.LeftArrow))
+    //     {
+    //         charDirection = Vector2.left;
+    //         directionValue = 1;
+    //         spriteRenderer.flipX = false;
+    //     }
+
+    //     else
+    //     {
+    //         charDirection = Vector2.zero;
+    //     }
+    // }
+
+    // public void GenerateMovement()
+    // {
+    //     rb.MovePosition(rb.position + charDirection * charMovingSpeed * Time.fixedDeltaTime);
+    //     animator.SetInteger("direction", directionValue);
+    // }
 }
