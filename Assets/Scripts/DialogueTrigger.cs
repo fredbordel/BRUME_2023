43c0845691265fd769a25f6d7314f9fdfb63d0df
Fred Bordel <frederique.bordeleau@gmail.com>
2023-09-10 18:01:55 -0400

    changed position that chien follows, force focus on next button dialogue when dialogue opens

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
     public GameObject dialogueBoxObject;
+    public GameObject nextButton;
+    private EventSystem eventSystem;
+
+
+  void OnEnable()
+    {
+        eventSystem = EventSystem.current;
+    }
 
    private void OnTriggerEnter2D()
     {
         dialogueBoxObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(nextButton, null);
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
