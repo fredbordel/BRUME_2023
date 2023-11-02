69d7ae6cb3e39fef6e2170052769770cd6b31933
Fred Bordel <frederique.bordeleau@gmail.com>
2023-11-02 19:01:47 -0400

    Add logic to manager open/close scene with joystick & conditional rendering for game menu

--- Assets/Scripts/ToggleGameMenu.cs
+++ Assets/Scripts/ToggleGameMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Localization.Settings;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class ToggleGameMenu : MonoBehaviour
@@ -11,10 +12,18 @@ public class ToggleGameMenu : MonoBehaviour
     [SerializeField]
     private GameObject menuFirstButton;
     private EventSystem eventSystem;
+    private PlayerInput playerInput;
+    private InputAction menuOpenCloseAction;
 
     void OnEnable()
     {
         eventSystem = EventSystem.current;
+    }
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        menuOpenCloseAction = playerInput.actions["MenuOpenClose"];
     }
     void Start()
     {
@@ -23,35 +32,26 @@ public class ToggleGameMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (menuOpenCloseAction.WasPressedThisFrame())
         {
-            if (gameMenu.activeSelf == true)
+            if (gameMenu.activeSelf)
             {
                 gameMenu.SetActive(false);
+                MainManager.Instance.IsDialogueOpened = false;
             }
-            else
+            else if (!gameMenu.activeSelf && !MainManager.Instance.IsDialogueOpened)
             {
                 gameMenu.SetActive(true);
                 eventSystem.SetSelectedGameObject(menuFirstButton, null);
+                MainManager.Instance.IsDialogueOpened = true;
             }
         }
     }
 
-    // private void Awake()
-    // {
-    //     if (Instance != null)
-    //     {
-    //         Destroy(gameObject);
-    //         return;
-    //     }
-
-    //     Instance = this;
-    //     DontDestroyOnLoad(gameObject);
-    // }
-
     public void ContinueGame()
     {
         gameMenu.SetActive(false);
+        MainManager.Instance.IsDialogueOpened = false;
     }
 
     public void QuitGame()
